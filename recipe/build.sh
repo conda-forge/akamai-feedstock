1f48901ae880a07b58627f1fff3815b25a63831b
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 18:25:33 -0800

    license fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" cli/main.go
 go-licenses save ./cli --save_path=license-files
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
