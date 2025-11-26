af8a39f5a16232e3baaa5b9435d39072bc9d4630
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 21:26:01 -0800

    licenses

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,7 +2,7 @@
 @setlocal EnableDelayedExpansion
 
 go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" cli/main.go || goto :error
-go-licenses save ./cli --save_path=license-files || goto :error
+go-licenses save ./cli --save_path=license-files --ignore github.com/inconshreveable/go-update || goto :error
 
 :: Manually copy licenses that go-licenses could not download
 xcopy /s %RECIPE_DIR%\license-files\* %SRC_DIR%\license-files || goto :error
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" cli/main.go
-go-licenses save ./cli --save_path=license-files
+go-licenses save ./cli --save_path=license-files --ignore github.com/inconshreveable/go-update
 
 # Manually copy licenses that go-licenses could not download
 cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
